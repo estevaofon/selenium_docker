b26b6f32151d6ef5ab071c6e3e900970e7197d43
Estevao <estevaopfon@gmail.com>
2022-07-04 15:49:52 -0300

    first commit

---
+++ app/script.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver import Remote
+from time import sleep
+
+sleep(5)
+
+driver = webdriver.Remote(
+    command_executor='http://selenium:4444/wd/hub',
+    options=webdriver.FirefoxOptions()
+)
+driver.get("http://www.python.org")
+assert "Python" in driver.title
+elem = driver.find_element(By.NAME, "q")
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+driver.save_screenshot("image.png")
+assert "No results found." not in driver.page_source
+assert "Estevao Rocks" in driver.page_source
+
+driver.close()
